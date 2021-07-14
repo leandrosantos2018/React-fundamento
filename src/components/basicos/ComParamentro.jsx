85f92c883bf3c13f163d462ab2bc01d2b74c8c38
leandro <leleuramos@gmail.com>
2021-07-14 19:26:22 -0300

    teste

--- src/components/basicos/ComParamentro.jsx
+++ src/components/basicos/ComParamentro.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export default function ComParametro(props){
 
     const status = props.nota >= 7 ? 'Aprovado' : 'Recuperação'
-    const notaInt = Math.ceil(props.nota)
+   // const notaInt = Math.ceil(props.nota)
   
     return (
         <div >
